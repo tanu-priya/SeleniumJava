1ca7f1df76670b90f161a38159c41b906a80b121
tanu-priya <tanu1priya@gmail.com>
2026-01-22 16:21:04 +0530

    Remove unused libs

--- src/main/java/com/inferenceCloud/pages/LoginPage.java
+++ src/main/java/com/inferenceCloud/pages/LoginPage.java
@@ -3,7 +3,6 @@ package com.inferenceCloud.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Wait;
 
 import com.inferenceCloud.utils.WaitUtil;
 
