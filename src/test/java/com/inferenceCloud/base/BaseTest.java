8e75d4bd52e91417ed70592bcdb51729632e3f5f
tanu-priya <tanu1priya@gmail.com>
2026-01-29 14:09:04 +0530

    Added Log4J

--- src/test/java/com/inferenceCloud/base/BaseTest.java
+++ src/test/java/com/inferenceCloud/base/BaseTest.java
@@ -1,16 +1,22 @@
 package com.inferenceCloud.base;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import com.inferenceCloud.driver.DriverFactory;
 import com.inferenceCloud.utils.ConfigReader;
+import com.inferenceCloud.utils.Log;
 
 public class BaseTest {
     protected String email;
     protected String password;
 
+    private Logger logger = Log.getLogger(BaseTest.class);
+
     @BeforeMethod
     public void setupDriver() {
-        DriverFactory.setDriver(ConfigReader.getProperty("browser"));  
+        logger.info("Setting up the driver and navigating to the URL");
+        DriverFactory.setDriver(ConfigReader.getProperty("browser"));          
+        logger.info("Launching URL: " + ConfigReader.getProperty("prodUrl"));
         DriverFactory.getDriver().manage().window().maximize();
         DriverFactory.getDriver().get(ConfigReader.getProperty("prodUrl"));
         email = ConfigReader.getProperty("email");
@@ -19,6 +25,7 @@ public class BaseTest {
 
     @AfterMethod
     public void tearDown() {
+        logger.info("Quitting Driver...");
         DriverFactory.unloadDriver();
       
     }
