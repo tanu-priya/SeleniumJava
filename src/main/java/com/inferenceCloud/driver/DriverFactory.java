49ca5bf6cb1655f962d1b7bc70ed1c64e4c8239c
tanu-priya <tanu1priya@gmail.com>
2026-02-05 18:17:27 +0530

    Added chrome options

--- src/main/java/com/inferenceCloud/driver/DriverFactory.java
+++ src/main/java/com/inferenceCloud/driver/DriverFactory.java
@@ -2,17 +2,23 @@ package com.inferenceCloud.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 
     private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
 
-   
+    public static WebDriver setDriver(String browser) {
 
-    public static WebDriver setDriver(String  browser) {
-        if(browser.equalsIgnoreCase("chrome")){
-            tlDriver.set(new ChromeDriver());
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new"); // IMPORTANT
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            tlDriver.set(new ChromeDriver(options));
             // set chrome driver
         } else if (browser.equalsIgnoreCase("firefox")) {
             // set firefox driver
@@ -21,18 +27,18 @@ public class DriverFactory {
 
         return getDriver();
 
-     }
+    }
 
-      public static WebDriver getDriver() {
+    public static WebDriver getDriver() {
         return tlDriver.get();
     }
 
     public static void unloadDriver() {
-        if(tlDriver.get() != null) {
+        if (tlDriver.get() != null) {
             tlDriver.get().quit();
             tlDriver.remove();
 
         }
     }
-    
+
 }
